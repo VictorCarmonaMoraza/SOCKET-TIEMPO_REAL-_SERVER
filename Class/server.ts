00d076355cae47ec506b176b36bf666bba469e4a
Victor <VictorCarmona9035@hotmail.com>
2023-11-29 13:42:57 +0100

    33 Escuchar emision desde el servidor

--- Class/server.ts
+++ Class/server.ts
@@ -44,7 +44,7 @@ export default class Server {
             console.log('Cliente conectado');
 
             //Mensajes
-            socket.mensaje(cliente);
+            socket.mensaje(cliente,this.io);
 
             //Desconectar
             socket.desconectar(cliente);
--- sockets/sockets.ts
+++ sockets/sockets.ts
@@ -1,4 +1,5 @@
 import { Socket } from 'socket.io';
+import socketIO from 'socket.io';
 //Aqui crearemos las acciones y configuraciones(logica de los sockets)
 
 
@@ -13,9 +14,11 @@ export const desconectar = (cliente: Socket) => {
 }
 
 //Escuchar mensaje
-export const mensaje = (cliente: Socket) => {
+export const mensaje = (cliente: Socket, io:socketIO.Server) => {
 
     cliente.on('mensaje', (payload: { de: string, cuerpo: string }) => {
         console.log('Mensaje recibido', payload);
+
+        io.emit('mensaje nuevo', payload);
     });
 }
